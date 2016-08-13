ed79d2d4006ba5967e5cccdcd32d59b444d1792d
Beadhangerone <denm.2206@gmail.com>
2016-08-14 00:24:30 +0300

    app controller updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
+  layout "application"
+  
   def render_403
     render file: "public/403.html", status: 403
   end
