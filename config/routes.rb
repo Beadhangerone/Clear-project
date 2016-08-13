3d5a1c7a45814e73d21e3a126ee7daaa386dddf9
Beadhangerone <denm.2206@gmail.com>
2016-08-13 11:56:16 +0300

    routes rb cleaned

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :items do
-    get :upvote,    on: :member
-    get :dnvote,    on: :member
-    get :expensive, on: :collection
-  end
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
